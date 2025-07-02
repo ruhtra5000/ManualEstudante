d08c0787eff1d2ef07653bf32130356288cfe886
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-07-02 01:11:38 -0300

    fix - consertando bug de limpeza do motor de forma repetida

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from experta import *
 from engine_parts.facts import *
 from engine_parts.engine_instance import ManualEngine
 from interface.home import homepageInicial
+import streamlit as st
 
 #Arquivo inicial que "limpa" o motor de inferencia
-#uma unica vez e renderiza a página inicial com streamlit
+#uma unica vez, cria algumas variáveis na sessão do streamlit, 
+#e renderiza a página inicial com streamlit
 
-ManualEngine.reset()
-    
+def inicializarAplicacao():
+    if "limpeza" not in st.session_state:
+        print("limpo")
+        st.session_state['limpeza'] = True
+        st.session_state["carregarPagina"] = True
+        ManualEngine.reset()
+
+inicializarAplicacao()
 homepageInicial()
 
