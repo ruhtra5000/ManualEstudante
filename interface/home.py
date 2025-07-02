96b14976831268cb467fa68bfb1612a33c6808a8
PacssauroRex <victorcauaorg@gmail.com>
2025-07-02 20:25:02 -0300

    Modificando a home da interface para incluir Apoio estudantil, Colação de grau e estagio

--- interface/home.py
+++ interface/home.py
@@ -5,11 +5,14 @@ from engine_parts.facts import *
 from interface.matricula import matriculaInicial
 from interface.disciplina import disciplinaInicial
 from interface.cancelamento import cancelamentoInicial
+from interface.apoioestudantil import apoioEstudantil
+from interface.colacaodegrau import colacaoGrau
+from interface.estagio import estagio
 
 #Função de carregamento da pagina inicial da aplicação
 def homepageInicial():
     st.title("Manual do Estudante")
-    aba_labels = ["Matrícula", "Disciplina", "Cancel. de Vínculo"]
+    aba_labels = ["Matrícula", "Disciplina", "Cancel. de Vínculo" , "Apoio Estudantil", "Colação de Grau", "Estágio"]
     abas = st.tabs(aba_labels)
 
     st.session_state["aba_ativa"] = "Matrícula"
@@ -30,6 +33,12 @@ def processarOpcao(opcao):
             disciplinaInicial()
         case "Cancel. de Vínculo":
             cancelamentoInicial()
+        case "Apoio Estudantil":
+            apoioEstudantil()
+        case "Colação de Grau":
+            colacaoGrau()
+        case "Estágio":
+            estagio()
             
     #No final, executa o motor — mas apenas se for a aba ativa
     if st.session_state.get("aba_ativa") == opcao:
