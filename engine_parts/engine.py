eb0ae2a3f24c8b357a024a328e3ee01c5022749c
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-07-02 04:42:36 -0300

    feat - criação das regras para disciplina no motor de inf.

--- engine_parts/engine.py
+++ engine_parts/engine.py
@@ -4,7 +4,23 @@ import streamlit as st
 
 #Motor de inferencia com definição das regras
 class Manual(KnowledgeEngine):
-    #Função para debug
+
+    #Construtor
+    def __init__(self):
+        super().__init__()
+        self.explicacao = [] #Armazena a explicabilidade
+
+    #Função que gera explicabilidade em texto
+    def gerarExplicacao(self):
+        expl = self.explicacao[-1]
+
+        txt = '**Explicação:**\n\n'
+        txt += f'Premissas: {expl['premissas']}\n\n'
+        txt += f'Fonte: {expl['fonte']}'
+
+        st.session_state['explicacao'] = txt
+
+    #Função que imprime os fatos (debug)
     def imprimirFatos(self):
         print("\nFatos atuais:")
         print(self.facts)
@@ -33,19 +49,193 @@ class Manual(KnowledgeEngine):
     #Disciplina
     @Rule(DisciplinaEntrada(txt = "cancelamento"))
     def cancelamentoDisciplina(self):
-        st.write("Em datas pré-estabelecidas, no Calendário Acadêmico, o(a) aluno(a) " +
-            "poderá requerer cancelamento de uma ou mais disciplinas. No caso de " +
-            "alunos diurnos, só poderão cancelar duas disciplinas do bloco principal.")
+        st.session_state['carregarPagina'] = 'cancelamentoDisciplina'
 
-        #"Explicabilidade"
-        st.info("Definido pela p.25 do Manual do Estudante 2023")
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'cancelamentoDisciplina',
+            'premissas': ['Aba disciplina', 'Busca por \'cancelamento\''],
+            'fonte': 'Pág. 25 do Manual do Estudande 2023',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
     
     @Rule(DisciplinaEntrada(txt = "dispensa"))
     def dispensaDisciplina(self):
-        '''st.header("Disciplina")
-        st.write("Disciplinas são legais")'''
-        #Falta adicionar a explicabilidade 
+        st.session_state['carregarPagina'] = 'dispensaDisciplina'
 
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"),
+        EdFisica(tipo = False)
+    )
+    def dispensaGeral(self):
+        st.session_state['carregarPagina'] = 'dispensaGeral'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaGeral',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Não é dispensa de educação física'],
+            'fonte': 'Pág. 25 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+    
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True)
+    )
+    def dispensaEdFisicaPergunta1(self):
+        st.session_state['carregarPagina'] = 'perguntaIdade'
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        Idade(valor=P(lambda v: v >= 30))
+    )
+    def dispensaPorIdade(self):
+        st.session_state['carregarPagina'] = 'dispensaPorIdade'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaPorIdade',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Maior de 30 anos de idade'],
+            'fonte': 'Pág. 25 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        Idade(valor=P(lambda v: v < 30))
+    )
+    def dispensaEdFisicaPergunta2(self):
+        st.session_state['carregarPagina'] = 'perguntaDeficiencia'
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        DeficienciaFisica(tipo = True)
+    )
+    def dispensaPorDeficiencia(self):
+        st.session_state['carregarPagina'] = 'dispensaPorDeficiencia'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaPorDeficiencia',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Tem deficiencia física'],
+            'fonte': 'Pág. 25 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        DeficienciaFisica(tipo = False)
+    )
+    def dispensaEdFisicaPergunta3(self):
+        st.session_state['carregarPagina'] = 'perguntaFilho'
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        MulherComFilhos(tipo = True)
+    )
+    def dispensaPorProle(self):
+        st.session_state['carregarPagina'] = 'dispensaPorProle'
+
+        self.declare(Sexo(valor = 'feminino'))
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaPorProle',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Mulher com filho(s)'],
+            'fonte': 'Pág. 26 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        MulherComFilhos(tipo = False)
+    )
+    def dispensaEdFisicaPergunta4(self):
+        st.session_state['carregarPagina'] = 'perguntaIncapacidadeRelativa'
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        IncapacidadeRelativa(tipo = True)
+    )
+    def dispensaIncapacidade(self):
+        st.session_state['carregarPagina'] = 'dispensaIncapacidade'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaIncapacidade',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Tem alguma incapacidade fisica temporaria ou relativa'],
+            'fonte': 'Pág. 26 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+    
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        IncapacidadeRelativa(tipo = False)
+    )
+    def dispensaEdFisicaPergunta5(self):
+        st.session_state['carregarPagina'] = 'perguntaEmprego'
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True), 
+        Emprego6h(tipo = True)
+    )
+    def dispensaPorEmprego(self):
+        st.session_state['carregarPagina'] = 'dispensaPorEmprego'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'dispensaPorEmprego',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Trabalha com jornada maior ou igual a 6h'],
+            'fonte': 'Pág. 26 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
+
+    @Rule(
+        DisciplinaEntrada(txt = "dispensa"), 
+        EdFisica(tipo = True),
+        Idade(valor=P(lambda v: v < 30)),
+        DeficienciaFisica(tipo = False),
+        MulherComFilhos(tipo = False),
+        IncapacidadeRelativa(tipo = False), 
+        Emprego6h(tipo = False)
+    )
+    def nenhumaDispensa(self):
+        st.session_state['carregarPagina'] = 'nenhumaDispensa'
+
+        #Explicabilidade
+        self.explicacao.append({
+            'nome': 'nenhumaDispensa',
+            'premissas': ['Aba disciplina', 'Busca por \'dispensa\'', 'Dispensa ed. física', 'Tem menos de 30 anos', 
+                          'Não tem deficiência física', 'Não tem filhos ou é mulher', 'Não tem incapacidade relativa ou temporal',
+                          'Não trabalha com jornada maior ou igual a 6h/dia'],
+            'fonte': 'Pág. 25-26 do Manual do Estudande 2023, e Resolução Nº 442/2006 CEPE/UFRPE',
+            'tempo': len(self.explicacao) + 1
+        })
+
+        self.gerarExplicacao()
 
     #Cancelamento de vínculo
     @Rule(CancelamentoEntrada(txt = "lorem ipsum"))
