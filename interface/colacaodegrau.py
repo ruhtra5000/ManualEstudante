88b76faf8579485ccda53138eca71a303b30a9ca
PacssauroRex <victorcauaorg@gmail.com>
2025-07-02 22:23:42 -0300

    Definição das interfaces para colacaoEmSeparado e colacaoAlunoLaureado

--- interface/colacaodegrau.py
+++ interface/colacaodegrau.py
@@ -12,9 +12,36 @@ def colacaoGrau():
     entrada = campoTexto(
         chave="colacao_grau",
         texto_label="Deseja saber algo mais sobre colação de grau?",
-        texto_placeholder="Ex.: colação em separado, aluno laureado"
+        texto_placeholder="Ex.: Colação em separado, Aluno laureado"
     )
 
     if entrada:
         ManualEngine.declare(ColacaoGrauEntrada(txt=entrada))
         ManualEngine.imprimirFatos()
+        ManualEngine.run()
+
+    match(st.session_state.get('carregarPagina')):
+        case 'colacaoEmSeparado':
+            colacaoEmSeparado()
+        case 'colacaoAlunoLaureado':
+            colacaoAlunoLaureado()
+
+#Interface para colação em separado
+def colacaoEmSeparado():
+    st.write("O(a) aluno(a) que precisar antecipar sua colação de grau poderá requerê-la em separado. " +
+            "Para tanto, o Coordenador do Curso deverá formalizar o requerimento de Colação de Grau, " +
+            "justificando (e documentando) o motivo. Este aluno irá receber o grau em sessão solene, " +
+            "em separado, presidida pelo Reitor ou seu representante, com a presença de pelo menos três " +
+            "professores do Curso. O(a) aluno(a) deverá estar presente na solenidade, na sua ausência, " +
+            "poderá ser substituído nesse ato por representante com procuração.")
+
+    st.info(st.session_state.get('explicacao'))
+
+#Interface para colação de grau de aluno laureado
+def colacaoAlunoLaureado():
+    st.write("O(a) aluno(a) que tiver o melhor coeficiente de rendimento escolar (média geral mais alta) " +
+            "e que não apresentar reprovações em seu Histórico Escolar, será conferido uma láurea e " +
+            "expedido um Certificado correspondente, entregue pelo Reitor da Universidade, durante a " +
+            "Cerimônia de Colação de Grau.")
+    
+    st.info(st.session_state.get('explicacao'))
